151dae9396675e59eead0df6aa01cf1a0f5c2252
haokai <haokai@gmail.com>
2023-10-04 14:03:12 +0800

    add origin

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import { NextResponse, NextRequest } from "next/server";
 
 const allowedOriginList =
   process.env.NODE_ENV === "production"
-    ? ["http://localhost:3000"]
-    : ["http://localhost:3000"];
+    ? ["http://localhost:3000", "https://auth-cookie-hazel.vercel.app"]
+    : ["http://localhost:3000", "https://auth-cookie-hazel.vercel.app"];
 
 export function middleware(req: NextRequest) {
   const origin = req.headers.get("origin");
