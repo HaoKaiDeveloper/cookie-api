e30fb86eb3c665f21d54ad3b0cbd9e539b7aa825
haokai <haokai@gmail.com>
2023-10-04 13:13:52 +0800

    upload

---
+++ app/api/login/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import { sign } from "jsonwebtoken";
+import { serialize } from "cookie";
+
+const MAX_AGE = 60 * 60;
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { name, email } = body;
+  const origin = req.headers.get("origin") as string;
+
+  if (!name || !email) {
+    return NextResponse.json({ msg: "failed" });
+  }
+  const token = sign({ name, email }, "jwtsecret", { expiresIn: MAX_AGE });
+
+  const seralized = serialize("token", token, {
+    httpOnly: true,
+    secure: true,
+    maxAge: MAX_AGE,
+    path: "/",
+  });
+
+  return NextResponse.json(
+    { msg: "login" },
+    {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": origin,
+        "Access-Control-Allow-Credentials": "true",
+        "Set-Cookie": seralized,
+      },
+    }
+  );
+}
+
+export async function OPTIONS(req: Request) {
+  const origin = req.headers.get("origin") as string;
+
+  return new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Credentials": "true",
+      "Accrss-Control-Allow-Headers": "content-type",
+      "Access-Control-Max-Age": "3600",
+    },
+  });
+}
---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse, NextRequest } from "next/server";
+
+const allowedOriginList =
+  process.env.NODE_ENV === "production"
+    ? ["http://localhost:3000"]
+    : ["http://localhost:3000"];
+
+export function middleware(req: NextRequest) {
+  const origin = req.headers.get("origin");
+
+  if (origin && !allowedOriginList.includes(origin)) {
+    return new NextResponse(null, {
+      status: 400,
+    });
+  }
+
+  if (req.nextUrl.pathname.startsWith("/secret")) {
+    const token = req.cookies.get("token")?.value;
+    if (!token) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+
+  return NextResponse.next();
+}
